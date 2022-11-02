5755266ac6a8af53e4d9020dd8b05c993053d624
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-11-02 19:07:56 -0300

    feat: uploading components

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,23 +1,30 @@
-import { Center, Icon, Text } from 'native-base'
-import Logo from '../assets/logo.svg'
-import { Button } from '../components/Button'
-import { Fontisto } from '@expo/vector-icons'
+import { Center, Icon, Text } from 'native-base';
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
+import { Fontisto } from '@expo/vector-icons';
+import { useAuth } from '../hooks/useAuth';
+import { NewPool } from './NewPool';
 
 export function SignIn() {
-  return (
-    <Center flex={1} bgColor="gray.900" p={7}>
-      <Logo width={212} height={40} />
-      <Button
-        title="Entrar com Google"
-        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
-        type="secondary"
-        mt={12}
-      />
+  const { signIn } = useAuth();
 
-      <Text color="white" textAlign="center" mt={4}>
-        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
-        de sua conta.
-      </Text>
-    </Center>
-  )
+  return <NewPool />;
+
+  // return (
+  //   <Center flex={1} bgColor="gray.900" p={7}>
+  //     <Logo width={212} height={40} />
+  //     <Button
+  //       title="Entrar com Google"
+  //       leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+  //       type="secondary"
+  //       mt={12}
+  //       onPress={signIn}
+  //     />
+
+  //     <Text color="white" textAlign="center" mt={4}>
+  //       Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
+  //       de sua conta.
+  //     </Text>
+  //   </Center>
+  // );
 }
