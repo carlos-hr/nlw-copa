767d3abbd6fe66391daa15a74295176319a18bc9
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-11-02 21:31:27 -0300

    feat: creating find pool page

--- mobile/src/screens/FindPool.tsx
+++ mobile/src/screens/FindPool.tsx
@@ -4,31 +4,24 @@ import Logo from '../assets/logo.svg';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
-export function NewPool() {
+export function FindPool() {
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header title="Criar novo bolão" />
+      <Header title="Criar novo bolão" showBackButton />
 
       <VStack mt={8} mx={5} alignItems="center">
-        <Logo />
-
         <Heading
           fontFamily="heading"
           color="white"
           fontSize="xl"
-          my={8}
+          mb={8}
           textAlign="center"
         >
-          Crie seu próprio bolão da copa {'\n'} e compartilhe entre amigos!
+          Encontre um bolão através de seu código único
         </Heading>
 
-        <Input mb={2} placeholder="Qual nome do seu bolão? " />
-        <Button title="Criar meu bolão" />
-
-        <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
-          Após criar seu bolão, você receberá um código único que poderá usar
-          para convidar outras pessoas.
-        </Text>
+        <Input mb={2} placeholder="Qual código do bolão?" />
+        <Button title="Buscar bolão" />
       </VStack>
     </VStack>
   );
