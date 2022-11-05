1b2ce9b32c1b7bd390874edf1194b4a1586afcf9
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-11-05 17:31:09 -0300

    feat: creating endpoint to join a pool

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -3,6 +3,7 @@ import { prisma } from '../lib/prisma';
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import ShortUniqueId from 'short-unique-id';
+import { authenticate } from '../plugins/authenticate';
 
 export async function poolRoutes(fastify: FastifyInstance) {
   fastify.get('/pools/count', async () => {
@@ -17,16 +18,93 @@ export async function poolRoutes(fastify: FastifyInstance) {
     });
 
     const { title } = createPoolBody.parse(req.body);
+
     const generate = new ShortUniqueId({ length: 6 });
     const code = String(generate()).toUpperCase();
 
-    await prisma.pool.create({
-      data: {
-        title,
-        code,
-      },
-    });
+    try {
+      await req.jwtVerify();
+
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+          ownerId: req.user.sub,
+          participants: {
+            create: {
+              userId: req.user.sub,
+            },
+          },
+        },
+      });
+    } catch {
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    }
 
     return res.status(201).send({ title, code });
   });
+
+  fastify.post(
+    '/pools/:id/join',
+    {
+      onRequest: [authenticate],
+    },
+    async (req, res) => {
+      const joinPoolBody = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPoolBody.parse(req);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participants: {
+            where: {
+              userId: req.user.sub,
+            },
+          },
+        },
+      });
+
+      if (!pool) {
+        return res.status(400).send({
+          message: 'Pool not found.',
+        });
+      }
+
+      if (pool.participants.length > 0) {
+        return res.status(400).send({
+          message: 'User already in the pool.',
+        });
+      }
+
+      if (!pool.ownerId) {
+        await prisma.pool.update({
+          where: {
+            id: pool.id,
+          },
+          data: {
+            ownerId: req.user.sub,
+          },
+        });
+      }
+
+      await prisma.participant.create({
+        data: {
+          poolId: pool.id,
+          userId: req.user.sub,
+        },
+      });
+
+      return res.status(201).send();
+    }
+  );
 }
