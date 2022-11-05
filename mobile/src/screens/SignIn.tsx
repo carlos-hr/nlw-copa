a41dbe6a2f951d1fb8eaa0b39c104c4803d5fdb5
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-11-04 21:08:09 -0300

    fix: adjusting signin page

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -3,28 +3,25 @@ import Logo from '../assets/logo.svg';
 import { Button } from '../components/Button';
 import { Fontisto } from '@expo/vector-icons';
 import { useAuth } from '../hooks/useAuth';
-import { Pools } from './Pools';
 
 export function SignIn() {
   const { signIn } = useAuth();
 
-  return <Pools />;
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="Entrar com Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="secondary"
+        mt={12}
+        onPress={signIn}
+      />
 
-  // return (
-  //   <Center flex={1} bgColor="gray.900" p={7}>
-  //     <Logo width={212} height={40} />
-  //     <Button
-  //       title="Entrar com Google"
-  //       leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
-  //       type="secondary"
-  //       mt={12}
-  //       onPress={signIn}
-  //     />
-
-  //     <Text color="white" textAlign="center" mt={4}>
-  //       Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
-  //       de sua conta.
-  //     </Text>
-  //   </Center>
-  // );
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
+        de sua conta.
+      </Text>
+    </Center>
+  );
 }
