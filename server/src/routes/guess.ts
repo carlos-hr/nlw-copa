2324d495e10ed16c14011ab438debe3a1bef3162
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-11-06 11:53:08 -0300

    feat: creating routes to add guesses

--- server/src/routes/guess.ts
+++ server/src/routes/guess.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { authenticate } from '../plugins/authenticate';
+import { z } from 'zod';
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get('/guesses/count', async () => {
@@ -7,4 +9,86 @@ export async function guessRoutes(fastify: FastifyInstance) {
 
     return { count };
   });
+
+  fastify.post(
+    '/pools/:poolId/games/:gameId/guesses',
+    {
+      onRequest: [authenticate],
+    },
+    async (req, res) => {
+      const createGuessParams = z.object({
+        poolId: z.string(),
+        gameId: z.string(),
+      });
+
+      const createGuesBody = z.object({
+        firstTeamPoints: z.number(),
+        secondTeamPoints: z.number(),
+      });
+
+      const { gameId, poolId } = createGuessParams.parse(req.params);
+      const { firstTeamPoints, secondTeamPoints } = createGuesBody.parse(
+        req.body
+      );
+
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_poolId: {
+            poolId,
+            userId: req.user.sub,
+          },
+        },
+      });
+
+      if (!participant) {
+        return res.status(400).send({
+          message: 'User is not in the pool.',
+        });
+      }
+
+      const guess = await prisma.guess.findUnique({
+        where: {
+          participantId_gameId: {
+            participantId: participant.id,
+            gameId,
+          },
+        },
+      });
+
+      if (guess) {
+        return res.status(400).send({
+          message: 'Participant already has a guess in the game.',
+        });
+      }
+
+      const game = await prisma.game.findUnique({
+        where: {
+          id: gameId,
+        },
+      });
+
+      if (!game) {
+        return res.status(400).send({
+          message: 'Game not found.',
+        });
+      }
+
+      if (game.date < new Date()) {
+        return res.status(400).send({
+          message: 'You can not send guesses after the game date.',
+        });
+      }
+
+      await prisma.guess.create({
+        data: {
+          gameId,
+          participantId: participant.id,
+          firstTeamPoints,
+          secondTeamPoints,
+        },
+      });
+
+      return res.status(201).send;
+    }
+  );
 }
--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -50,7 +50,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
   });
 
   fastify.post(
-    '/pools/:id/join',
+    '/pools/join',
     {
       onRequest: [authenticate],
     },
@@ -105,6 +105,102 @@ export async function poolRoutes(fastify: FastifyInstance) {
       });
 
       return res.status(201).send();
+    }
+  );
+
+  fastify.get(
+    '/pools',
+    {
+      onRequest: [authenticate],
+    },
+    async (req) => {
+      const pools = await prisma.pool.findMany({
+        where: {
+          participants: {
+            some: {
+              userId: req.user.sub,
+            },
+          },
+        },
+        include: {
+          owner: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+
+          participants: {
+            select: {
+              id: true,
+
+              user: {
+                select: {
+                  avatarUrl: true,
+                },
+              },
+            },
+            take: 4,
+          },
+
+          _count: {
+            select: {
+              participants: true,
+            },
+          },
+        },
+      });
+
+      return pools;
+    }
+  );
+
+  fastify.get(
+    '/pools/:id',
+    {
+      onRequest: [authenticate],
+    },
+    async (req) => {
+      const getPoolParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPoolParams.parse(req.params);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          owner: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+
+          participants: {
+            select: {
+              id: true,
+
+              user: {
+                select: {
+                  avatarUrl: true,
+                },
+              },
+            },
+            take: 4,
+          },
+
+          _count: {
+            select: {
+              participants: true,
+            },
+          },
+        },
+      });
+
+      return pool;
     }
   );
 }
